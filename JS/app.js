a8783b0e6150a11a4ba999759aafe430c192e9c6
Ganesh kale <ganeshdoitnowh100@gmail.com>
2023-09-15 21:08:41 +0530

    Changes in app.js

--- JS/app.js
+++ JS/app.js
@@ -1,28 +1,9 @@
-console.log("Here we go");
-let results = {
-  tag: "",
-  free: true,
-  role: false,
-  user: "akshaykumarcodewithharry",
-  email: "akshaykumarcodewithharry@gmail.com",
-  score: 0.48,
-  state: "undeliverable",
-  domain: "gmail.com",
-  reason: "invalid_mailbox",
-  mx_found: true,
-  catch_all: null,
-  disposable: false,
-  smtp_check: false,
-  did_you_mean: "",
-  format_valid: true,
-};
-
 let btn = document.querySelector(".btn");
 btn.addEventListener("click", async (e) => {
   e.preventDefault();
   let rCont = document.querySelector("#resultCont");
   rCont.innerHTML = `<img width="123" src="img/loading.svg">`;
-  const key = "ema_live_TG91jqTr9aUTzGycMiqt1JUc0kHWJqV3ECJ9iNtl";
+  const key = ""; //Place your key here
   let email = document.getElementById("email").value;
   let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`;
   let res = await fetch(url);
